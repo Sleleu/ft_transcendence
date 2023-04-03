1e8603c8c04d774cd9abbcb6605edf39af24647f
Gottiee <gottiedev@gmail.com>
2023-04-03 19:17:13 +0200

    name component doe

--- srcs/requirements/frontend/src/components/home/Name.tsx
+++ srcs/requirements/frontend/src/components/home/Name.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Name.css' 
+import RankBarUnderName from './RankBarUnderName'
 
 type NameProps = {
   name: string;
@@ -7,18 +8,29 @@ type NameProps = {
   rank: string;
 }
 
+const NamePars = (name: string) => {
+  if (name.length > 13)
+  {
+    return name.slice(0, 10) + '...'
+  }
+  return name
+}
+
 const Name = ({name, elo, rank}: NameProps) => {
   return (
     <div className='containerName'>
       <div className='containerRoseName'>
         <div className='containerTextName'>
-          {/* <h1 className='nameText'>{name}</h1> */}
-          <h1 className='nameText'>gottieeeeeeeeeeeee</h1>
+          <h1 className='nameText'>{NamePars(name)}</h1>
         </div>
+        <RankBarUnderName
+        elo={elo}
+        rank={rank}
+        />
       </div>
-      {/* j'ai mute le css des brush parce que c'est de la merde */}
+      {/* j'ai mute les brush parce que c'est de la merde
       <div className='brushTop'/>
-      <div className='brushBot'/>
+      <div className='brushBot'/> */}
     </div>
   )
 }
