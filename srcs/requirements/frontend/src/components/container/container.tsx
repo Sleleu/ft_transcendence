6734a1b39d7b811d54b4ee6f69aa145bc8b1d62d
Sebastien LELEU <leleu.sebastien@hotmail.fr>
2023-04-04 17:01:14 +0200

    continue settings components adaptation in react

--- srcs/requirements/frontend/src/components/container/container.tsx
+++ srcs/requirements/frontend/src/components/container/container.tsx
@@ -2,27 +2,37 @@ import { CSSProperties, Component } from 'react';
 
 interface Props {
 	width?: string;
+	minWidth?: string;
 	height?: string;
+	minHeight?: string;
 	padding?: string;
+	margin?: string;
 	backgroundColor?: string;
 	borderRadius?: string;
+	borderBottom?: string;
+	borderRight?: string;
+	direction?: "row" | "column";
 	children?: React.ReactNode;
 }
 
 class Cont extends Component<Props> {
 	render() {
-		const {backgroundColor, padding, width, height, borderRadius, children} = this.props;
+		const {backgroundColor, padding, margin, direction, width, minWidth, height, borderBottom, borderRight, minHeight, borderRadius, children} = this.props;
 
 		const ContStyle: CSSProperties = {
-			backgroundColor: backgroundColor ? backgroundColor : 'black',
+			backgroundColor: backgroundColor ? backgroundColor : 'none',
 			padding: padding ? padding : '5px',
-    		opacity: 0.75,
-    		width: width ? width : '679px',
-   			height: height ? height : '425px',
-    		borderRadius: borderRadius ? borderRadius : '15px',
+			margin: margin ? margin : 'none',
+    		width: width ? width : 'none',
+   			height: height ? height : 'none',
+    		borderRadius: borderRadius ? borderRadius : 'none',
+			borderBottom: borderBottom? borderBottom : 'none',
+			borderRight: borderRight ? borderRight : 'none',
 			display: "flex",
-			flexDirection: "row",
-			alignItems: "flex-start"
+			flexDirection: direction ? direction : "column",
+			alignItems: "flex-start",
+			minWidth: minWidth ? minWidth : 'none',
+			minHeight: minHeight ? minHeight :'none'
 		}
 		return (
 			<div style={ContStyle}> {children}</div>
@@ -32,15 +42,16 @@ class Cont extends Component<Props> {
 
 class HeaderBar extends Component<Props> {
 	render() {
-		const {backgroundColor, padding, width, height, borderRadius, children} = this.props;
+		const {backgroundColor, padding, width, height, borderRadius, borderBottom, children} = this.props;
 
 		const HeaderStyle: CSSProperties = {
-			backgroundColor: backgroundColor ? backgroundColor : 'black',
+			backgroundColor: backgroundColor ? backgroundColor : 'none',
 			padding: padding ? padding : '5px',
-    		opacity: 0.75,
-    		width: width ? width : '679px',
+    		opacity: 1,
+    		width: width ? width : '99%',
    			height: height ? height : '70px',
-    		borderRadius: borderRadius ? borderRadius : '15px',
+    		borderRadius: borderRadius ? borderRadius : 'none',
+			borderBottom: borderBottom? borderBottom : 'none',
 			display: "flex",
 			flexDirection: "row",
 			alignItems: "flex-start"
--- srcs/requirements/frontend/src/components/settings/Settings.tsx
+++ srcs/requirements/frontend/src/components/settings/Settings.tsx
@@ -1,11 +1,70 @@
-import React from 'react';
+import React, { CSSProperties, Component } from 'react';
 import './Settings.css'
 import '../../assets/css/Text.css'
+import { useNavigate } from 'react-router-dom';
+
 import settings_svg from '../../assets/img/settings.svg';
 import return_svg from '../../assets/img/return.svg';
 import frog_svg from '../../assets/img/frog.svg';
+import {Cont, HeaderBar} from '../container/container'
 
-function Home() {
+type Props = {
+	text: string;
+}
+
+const SettingsTitle = () => {
+	return (
+		<div className='display-settings'>
+			<div className='symbol'>
+				<img src={settings_svg} className='vector-neon'/>
+			</div>
+			<div>
+				<p className='text big bold cyan-stroke'>Settings</p>
+			</div>
+		</div>
+	);
+}
+
+const ReturnButton = () => {
+	const navigate = useNavigate();
+	return (
+		<button className='btn-return' onClick={() => navigate("/")}>
+			<img src={return_svg} className='vector-neon-return'/>
+		</button>
+	);
+}
+
+const SettingsOptions = () => {
+	const navigate = useNavigate();
+	return (
+		<React.Fragment>
+		<Cont padding='0px' width='45%' height='39px' borderBottom='1px solid white'>
+			<Option text='Profile'/>
+		</Cont>
+		<Cont  padding='0px' width='95%' height='40px' borderBottom='1px solid white'>
+			<Option text='Confidentiality'/>
+		</Cont>
+		</React.Fragment>
+	);
+}
+
+const Option = ({text}: Props) => {
+	return (
+		<div style={{ marginTop: '-28px' }}>
+    		<p className='text big bold purple-stroke'>{text}</p>
+		</div>
+	);
+}
+
+const ButtonOption = () => {
+	return (
+		<React.Fragment>
+			
+		</React.Fragment>
+	);
+}
+
+function Settings() {
     return (
         <div className="Home">
 			<div className="settings">
@@ -42,8 +101,21 @@ function Home() {
 					</div>
 				</div>
 			</div>
+			<Cont width='50%' height='30%' direction='column' borderRadius='15px' backgroundColor='rgba(0, 0, 0, 0.75)' minWidth='679px' minHeight='425px'>
+				<HeaderBar borderBottom='1px solid #ffffff'>
+					<SettingsTitle /> <ReturnButton />
+				</HeaderBar>
+				<Cont backgroundColor='none' width='100%' direction='row'>
+					<Cont backgroundColor='none' borderRight='1px solid #ffffff' width='220px' height='94%' borderRadius='15px' >
+						<SettingsOptions/>
+					</Cont>
+					<Cont backgroundColor='none' minWidth='270px' minHeight='340px' width='100%' height='98%'>
+
+					</Cont>
+				</Cont>
+			</Cont>
         </div>
     );
 }
 
-export default Home;
+export default Settings;
