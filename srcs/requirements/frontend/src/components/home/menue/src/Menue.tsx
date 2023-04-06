a6cc508813ddb9dfcc0c80a92769909d93ccb82a
Gottiee <gottiedev@gmail.com>
2023-04-06 16:38:00 +0200

    uptdata menue

--- srcs/requirements/frontend/src/components/home/menue/src/Menue.tsx
+++ srcs/requirements/frontend/src/components/home/menue/src/Menue.tsx
@@ -11,29 +11,42 @@ const Menue:FC<MenueProps> = ({changeComponent}) => {
     <div className='containerMenue'>
         <div className='containerLeftMenue'>
             <div className='containerTop'>
-                <div className='containerFriend'>
+                <div 
+                className='containerFriend'
+                onClick={() => changeComponent('friend')}
+                >
                     <div className='friendLogo' />
                     <h3 className='friendTxt'>friends</h3>
                 </div>
-                <div className='containerStat'>
+                <div className='containerStat'
+                onClick={() => changeComponent('stat')}
+                >
                     <div className='statLogo' />
                     <h3 className='statTxt'>stat</h3>
                 </div>
-                <div className='containerLeader'>
+                <div className='containerLeader'
+                onClick={() => changeComponent('leader')}
+                >
                     <div className='leaderLogo' />
                     <h3 className='leaderTxt'>Leader</h3>
                 </div>
             </div>
             <div className='containerBot'>
-                <div className='containerSettings'>
+                <div className='containerSettings'
+                onClick={() => changeComponent('settings')}
+                >
                     <div className='settingsLogo' />
                     <h3 className='settingsTxt'>settings</h3>
                 </div>
-                <div className='containerChat'>
+                <div className='containerChat'
+                onClick={() => changeComponent('chat')}
+                >
                     <div className='chatLogo' />
                     <h3 className='chatTxt'>chat</h3>
                 </div>
-                <div className='containerHistoric'>
+                <div className='containerHistoric'
+                onClick={() => changeComponent('historic')}
+                >
                     <div className='historicLogo' />
                     <h3 className='historicTxt'>historic</h3>
                 </div>
