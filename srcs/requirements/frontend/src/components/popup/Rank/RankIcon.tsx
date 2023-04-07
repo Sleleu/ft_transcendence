f0c8fa42c1945a838b72fc96ea21f99287448032
GMansuy <gabriel.mansuy@gmail.com>
2023-04-07 13:56:59 +0200

    changed icon styles when hover

--- srcs/requirements/frontend/src/components/popup/Rank/RankIcon.tsx
+++ srcs/requirements/frontend/src/components/popup/Rank/RankIcon.tsx
@@ -20,6 +20,52 @@ interface Props {
 
 const RankIcon:React.FC<Props> = ({user, icon, scale=1, y=0, x=0, changeComponent}) => {
 
+    const [isHovered, setIsHovered] = useState<boolean>(false);
+
+    let display:string;
+    if (icon)
+        display = icon;
+    else if (user)
+        display = user.rank;
+    else
+        return (null);
+
+
+    const handleHover = () => {
+        setIsHovered(!isHovered);
+    };
+    
+    const handleClick = () =>{
+        if (changeComponent)
+            changeComponent(display + 'Lead')
+    }
+
+    let auraColor: string;
+    switch (display) {
+        case 'bronze':
+            auraColor = 'brown';
+            break;
+        case 'silver':
+            auraColor = 'white';
+            break;
+        case 'gold':
+            auraColor = 'gold';
+            break;
+        case 'crack':
+            auraColor = 'pink';
+            break;
+        case 'ultime':
+            auraColor = 'red';
+            break;
+        default:
+            auraColor = 'white';
+            break;
+    }
+    const Container: CSSProperties = {
+        cursor: isHovered ? 'pointer' : 'auto',
+        filter: isHovered ? 'drop-shadow(0px 0px 30px '+ auraColor +')': 'none',
+        transition: 'all 0.3s ease-in-out',
+    }
     const bronzePic: CSSProperties = {
         height: (250 * scale) + 'px',
         width: (170 * scale) + 'px',
@@ -27,6 +73,7 @@ const RankIcon:React.FC<Props> = ({user, icon, scale=1, y=0, x=0, changeComponen
         marginTop: (-45 - y) + 'px',
         marginLeft: x + 'px',
         paddingRight: '5px',
+        
     }
     const silverPic: CSSProperties = {
         height: (250 * scale) + 'px',
@@ -60,22 +107,9 @@ const RankIcon:React.FC<Props> = ({user, icon, scale=1, y=0, x=0, changeComponen
         marginLeft: x + 'px',
         paddingRight: '5px',
     }
-    
-    let display:string;
-    if (icon)
-        display = icon;
-    else if (user)
-        display = user.rank;
-    else
-        return (null);
-
-    const handleClick = () =>{
-        if (changeComponent)
-            changeComponent(display + 'Lead')
-    }
 
   return (
-    <div onClick={handleClick}>
+      <div onClick={handleClick} onMouseEnter={handleHover} onMouseLeave={handleHover} style={Container}>
         {display === 'bronze' && <img src={bronze} alt='bronze' style={bronzePic}/>}
         {display === 'silver' && <img src={silver} alt='silver' style={silverPic}/>}
         {display === 'gold' && <img src={gold} alt='gold' style={goldPic}/>}
