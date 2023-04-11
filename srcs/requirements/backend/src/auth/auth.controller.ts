629afd251462056213f1c3ba0ce33fb139fc5061
Sebastien LELEU <leleu.sebastien@hotmail.fr>
2023-04-11 19:52:27 +0200

    begin signin

--- srcs/requirements/backend/src/auth/auth.controller.ts
+++ srcs/requirements/backend/src/auth/auth.controller.ts
@@ -7,15 +7,15 @@ export class AuthController{
 	constructor(private authService: AuthService) {}
 
 	@Post('signup')
-	signup(
-		@Body() dto: AuthDto) {
+	signup(@Body() dto: AuthDto) {
 		console.log(dto)
 		return this.authService.signup(dto);
 	}
 
 
 	@Post('signin')
-	signin() {
-		return this.authService.signin();
+	signin(@Body() dto: AuthDto) {
+		console.log(dto)
+		return this.authService.signin(dto);
 	}
 }
--- srcs/requirements/backend/src/auth/auth.service.ts
+++ srcs/requirements/backend/src/auth/auth.service.ts
@@ -14,20 +14,23 @@ export class AuthService{
 	}
 
 	async signup(dto: AuthDto) {
+		
 		// generer le hash
 		const hash = await argon.hash(dto.password);
-		// save the new user in the db
+		
+		// tenter de save l'user
 		try {
-			const user = await this.prisma.user.create({
-				data: {
-					username: dto.username,
-					hash
-				},
-			});
+				const user = await this.prisma.user.create({
+					data: {
+						username: dto.username,
+						hash
+					},
+				});
 
-			// return the saved user
-			return (user);
-			} catch (error) {
+				// return the saved user
+				return (user);
+			}
+		catch (error) {
 				if (error instanceof Prisma.PrismaClientKnownRequestError) {
 					if (error.code === 'P2002') {
 						throw new ForbiddenException('Username already taken',)
@@ -39,7 +42,25 @@ export class AuthService{
 			}
 	}
 
-	signin() {
+	async signin(dto: AuthDto) {
+
+		// Find user by username
+		const user = await this.prisma.user.findUnique({
+			where: {
+				username: dto.username
+			}
+		})
+		
+		// if user does not exist throw exception
+		if (!user)
+			throw new ForbiddenException('Incorrect username or password') // ne pas preciser lequel des deux
+		
+		// compare password
+		const passwordMatch = await argon.verify(user.hash, dto.password);
+		if (!passwordMatch)
+			throw new ForbiddenException('Incorrect username or password')
+
+		// sent back the user
 		return ({msg: 'I have signed in'});
 	}
 }
