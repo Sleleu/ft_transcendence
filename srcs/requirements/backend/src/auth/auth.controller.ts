0d630c25accc0255a2512f968beba51ff9544972
Sebastien LELEU <leleu.sebastien@hotmail.fr>
2023-04-11 17:37:11 +0200

    add dto

--- srcs/requirements/backend/src/auth/auth.controller.ts
+++ srcs/requirements/backend/src/auth/auth.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from './dto/auth.dto'
 
 @Controller('auth')
 export class AuthController{
 	constructor(private authService: AuthService) {}
 
-	@Post('signup')
-	signup() {
+	@Get('signup')
+	signup(@Body() dto: AuthDto) {
+		console.log({
+			dto,
+		});
 		return this.authService.signup();
 	}
 
