6c41159438ae7ca7bc6b8b1ef6e39f8c05f67f64
Sebastien LELEU <leleu.sebastien@hotmail.fr>
2023-04-21 16:57:58 +0200

    fix error bug on intra login

--- srcs/requirements/backend/src/intra/intra.service.ts
+++ srcs/requirements/backend/src/intra/intra.service.ts
@@ -1,5 +1,5 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable, Logger } from '@nestjs/common';
+import { ForbiddenException, Injectable, Logger } from '@nestjs/common';
 import { AxiosError} from 'axios';
 import { catchError } from 'rxjs';
 import { AuthDto } from 'src/auth/dto/auth.dto';
@@ -8,16 +8,14 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
 export class IntraService {
 	constructor	(private prisma: PrismaService,
-				 private readonly httpService: HttpService,
-				 private readonly logger = new Logger(IntraService.name)) {}
+				 private readonly httpService: HttpService) {}
 
 	async login(dto: AuthDto)
 	{
 		const data = this.httpService
 		  .get('https://api.intra.42.fr/oauth/token').pipe(
 			catchError((error: AxiosError) => {
-			  this.logger.error(error.response?.data);
-			  throw 'Error ' + error + ' happened!';
+			  throw new ForbiddenException(error.message);
 			}),
 		  );
 		console.log(data.pipe.name);
