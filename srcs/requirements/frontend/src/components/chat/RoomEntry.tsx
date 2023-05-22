c360bb234acd0b6b82a80f53d98b61c546bccc6f
Gabriel MANSUY <gmansuy@e2r7p7.clusters.42paris.fr>
2023-05-22 11:39:23 +0200

    front roomSelect started

--- srcs/requirements/frontend/src/components/chat/RoomEntry.tsx
+++ srcs/requirements/frontend/src/components/chat/RoomEntry.tsx
@@ -17,16 +17,51 @@ interface Props {
 
 const RoomEntry:React.FC<Props> = ({room, handleSelect}) => {
 
-const RoomsStyle: CSSProperties = {
-    alignSelf: 'center', margin : '5px', padding: '10px',
-    color: '#fff',
-    border: '1px solid white',
-	borderRadius: '30px',
+const RoomsContainer: CSSProperties = {
+  margin: '7px',
+  color: '#fff', fontSize:'18px',
+  border: '1px solid white', borderRadius: '30px',
+  cursor: 'pointer',
+  display: 'flex', justifyContent: 'space-around',
 }
-
+const legend: CSSProperties = {
+  fontSize: '14px', fontStyle: 'italic',
+}
+const Owner: CSSProperties = {
+  color:'#ee3388',
+}
+const Salon: CSSProperties = {
+  fontWeight: '600', color:'#00ccdd',
+}
+const Type: CSSProperties = {
+  color: (room.type === 'public') ? '#00ee13'
+  : (room.type === 'protected') ? '#eeaa00'
+  : '#ff0000',
+}
+const InSalon: CSSProperties = {
+  color:'#ffdd77',
+}
+const Block: CSSProperties = {
+  display: 'flex', flexDirection:'column', justifyContent: 'space-around',
+}
   return (
-    <div style={RoomsStyle} onClick={() => handleSelect(room.id, room.name, room.type)}>
-      {room.name}
+    <div style={RoomsContainer} onClick={() => handleSelect(room.id, room.name, room.type)}>
+      <div style={Block}>
+      <span style={legend}>Salon Name</span>
+        <span style={Salon}>{room.name}</span>
+      </div>
+      <div style={Block}>
+        <span style={legend}>Owner</span>
+        <span style={Owner}>ownerNameHere</span>
+      </div>
+      <div style={Block}>
+        <span style={legend}>Status</span>
+        <span style={Type}>{room.type.toUpperCase()}</span>
+      </div>
+      <div style={Block}>
+        <span style={legend}>In salon</span>
+        <span style={InSalon}>-42</span>
+      </div>
     </div>
   )
 }
--- srcs/requirements/frontend/src/components/chat/RoomSelect.tsx
+++ srcs/requirements/frontend/src/components/chat/RoomSelect.tsx
@@ -6,6 +6,8 @@ import CreateRoom from './CreateRoom';
 import { User } from '../types';
 import Room from './RoomEntry';
 import RoomEntry from './RoomEntry';
+import retLogo from '../../img/navBar/returnLogo.png'
+import firendLogo from '../../img/menue/friend.png'
 
 interface Room {
     name: string;
@@ -91,6 +93,15 @@ const RoomSelect:React.FC<Props> = ({user}) => {
         flexDirection: 'column',
         justifyContent: 'space-around',
     }
+    const navBarChat: CSSProperties = {
+        height:'10%',
+
+        // backgroundColor: 'red',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+    }
+
     const displayBox: CSSProperties = {
         width: '90%', height: '80%',
         margin: '10px',
@@ -99,21 +110,33 @@ const RoomSelect:React.FC<Props> = ({user}) => {
         border: '2px solid #fff', backgroundColor: '#000',
         display: 'flex',
         flexDirection: 'column', justifyContent: 'center',
-        wordWrap: 'break-word',
+        overflow: 'auto',
     }
-    const RoomsStyle: CSSProperties = {
-        alignSelf: 'center', margin : '5px', padding: '10px',
-        color: '#fff',
-        border: '1px solid white',
-        borderRadius: '30px',
-        cursor: hover ? 'pointer':'auto',
-    }
-    const buttonCreateRoom: CSSProperties = {
-        width: '20%', height : '10%',
+    const buttons: CSSProperties = {
+        width: '20%', height : '80%',
         borderRadius: '30px', alignSelf: 'center',
         border: '2px solid #fff', backgroundColor: '#000',
         color: '#fff', fontWeight: '800', fontSize: '18px',
-        cursor: hover ? 'pointer':'auto',
+        cursor: 'pointer',
+    }
+    const returnButton: CSSProperties = {
+        width: '10%', height : '80%',
+        borderRadius: '30px', alignSelf: 'center',
+        border: '2px solid #fff', backgroundColor: '#000',
+        color: '#fff', fontWeight: '800', fontSize: '18px',
+        cursor: 'pointer',
+    }
+    const returnLogo : CSSProperties = {
+        marginLeft: '30%',
+        marginTop: '10px',
+        width: '35%',
+        minWidth:'33px',
+    }
+    const friendLogo : CSSProperties = {
+        marginLeft: '27%',
+        marginTop: '10px',
+        width: '35%',
+        minWidth:'33px',
     }
 
     const handleSelect = (id: number, roomName: string, type: string) => {
@@ -143,16 +166,19 @@ const RoomSelect:React.FC<Props> = ({user}) => {
                 createRoom ? <CreateRoom socket={socket} setRooms={setRooms} setCreateRoom={setCreateRoom} user={user}/> :
             <div style={middleBlock}>
                 <div style={leftBlock}>
-                    <button style={buttonCreateRoom} onMouseEnter={handleHover} onMouseLeave={handleHover}
+                    <div style={navBarChat}>
+                        <button style={buttons}
+                        >PUBLIC SALON</button>
+                        <button style={buttons}
                         onClick={() => setCreateRoom(true)}>CREATE ROOM</button>
+                        <div style={returnButton}>
+                            <img src={firendLogo} style={friendLogo}></img>
+                        </div>
+                        <div style={returnButton}>
+                            <img src={retLogo} style={returnLogo}></img>
+                        </div>
+                    </div>
                     <div style={displayBox}>
-                        {/* {rooms.map((room) => <div
-                            style={RoomsStyle}
-                            key={room.id}
-                            onClick={() => handleSelect(room.id, room.name, room.type)}
-                            onMouseEnter={handleHover}
-                            onMouseLeave={handleHover}
-                            >{room.name}</div>)} */}
                         {rooms.map((room) => <RoomEntry room={room} key={room.id} handleSelect={handleSelect}/>)}
                     </div>
                 </div>
