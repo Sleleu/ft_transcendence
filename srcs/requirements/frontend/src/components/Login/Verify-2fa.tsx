595d15e3cfdd920e5f3507162278396ca79cb3cd
Sebastien LELEU <leleu.sebastien@hotmail.fr>
2023-05-29 16:46:23 +0200

    begin 2fa verify page

--- srcs/requirements/frontend/src/components/Login/Verify-2fa.tsx
+++ srcs/requirements/frontend/src/components/Login/Verify-2fa.tsx
@@ -1,5 +1,51 @@
-import React, { useState, ChangeEvent, FormEvent } from "react";
+import React, { useState } from "react";
 import { enableTwoFAVerified, verifyTwoFACode } from "../Api";
+import AuthCode from 'react-auth-code-input';
+import { CSSProperties } from "styled-components";
+import './Login.css'
+
+const Container: CSSProperties = {
+	height: '70%',
+	width: '30%',
+	backgroundColor: 'rgba(0, 0, 0, 0.8)',
+	borderRadius: '30px',
+	display: 'flex',
+	flexDirection: 'column',
+	justifyContent: 'center',
+	alignItems: 'center',
+	fontWeight: '1000',
+	fontFamily: 'Montserrat, sans-serif',
+	color: '#FFFFFF',
+	textAlign: 'center',
+	textShadow: '0 0 8px rgba(255, 255, 255, 0.8)',
+	padding: '20px',
+};
+
+const Form: CSSProperties = {
+	width: '80%',
+	margin: '20px 0',
+};
+
+const Button: CSSProperties = {
+	backgroundColor: '#4CAF50',
+	color: 'white',
+	padding: '15px 32px',
+	textAlign: 'center',
+	textDecoration: 'none',
+	display: 'inline-block',
+	fontSize: '16px',
+	margin: '4px 2px',
+	cursor: 'pointer',
+	border: 'none',
+	borderRadius: '4px',
+};
+
+const AuthCodeContainer: CSSProperties = {
+	display: 'flex',
+	justifyContent: 'center',
+	margin: '20px 0',
+	flexDirection: 'row'
+  };
 
 interface Verify2FAProps {
   onVerifySuccess: () => void;
@@ -10,11 +56,11 @@ const Verify2FA: React.FC<Verify2FAProps> = (props) => {
   const [error, setError] = useState<string | null>(null);
   const [message, setMessage] = useState<string | null>(null);
 
-  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setCode(event.target.value);
+  const handleInputChange = (code: string) => {
+    setCode(code);
   };
 
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     try {
@@ -29,18 +75,26 @@ const Verify2FA: React.FC<Verify2FAProps> = (props) => {
   };
 
   return (
-    <div>
-      <h1>Verify 2FA</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Enter your 2FA code:
-          <input type="text" onChange={handleInputChange} value={code} />
-        </label>
-        <button type="submit">Verify</button>
-      </form>
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      {message && <p style={{ color: "green" }}>{message}</p>}
-    </div>
+	<div className="baground">
+	  <div className='containerFullPage'>
+		<div style={Container}>
+	      <h1>Verify 2FA</h1>
+	      <form onSubmit={handleSubmit} style={Form}>
+	        <label>
+	          Enter your 2FA code:
+	        </label>
+	        <div style={AuthCodeContainer}>
+	          <AuthCode containerClassName="auth-code-container" inputClassName="auth-code-input-cell" allowedCharacters="numeric" onChange={handleInputChange}/>
+	        </div>
+	        <button type="submit" style={Button}>Verify</button>
+	      </form>
+	      <div style={{width: '80%'}}>
+	        {error && <p style={{ color: "red" }}>{error}</p>}
+	        {message && <p style={{ color: "green" }}>{message}</p>}
+	      </div>
+	    </div>
+	  </div>
+	</div>
   );
 }
 
