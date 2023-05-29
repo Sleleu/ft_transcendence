4acfea0298d42cf5c4a22a2a4653975ba2fbc50c
Sebastien LELEU <leleu.sebastien@hotmail.fr>
2023-05-29 15:53:10 +0200

    create PrivateRoute front page

--- srcs/requirements/frontend/src/PrivateRoute.tsx
+++ srcs/requirements/frontend/src/PrivateRoute.tsx
@@ -1,5 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './components/home/Home.css'
+import { CSSProperties } from 'styled-components';
+import LogoutImage from './img/logout.png'
+
+const Container: CSSProperties = {
+	height: '70%',
+	width: '30%',
+	backgroundColor: 'rgba(0, 0, 0, 0.8)',
+	borderRadius: '30px',
+	display: 'flex',
+	flexDirection: 'column',
+	justifyContent: 'center',
+	alignItems: 'center',
+	fontWeight: '1000',
+	fontFamily: 'Montserrat, sans-serif',
+	color: '#FFFFFF',
+	textAlign: 'center',
+	textShadow: '0 0 8px rgba(255, 255, 255, 0.8)',
+  };
+
+const Text: CSSProperties = {
+  fontSize: '24px',
+  maxWidth: '90%',
+  wordWrap: 'break-word',
+};
+
+const Image: CSSProperties = {
+	maxWidth: '100%',
+  };
 
 const PrivateRoute: React.FC<{ children: React.ReactElement }> = ({ children }) => {
   const navigate = useNavigate();
@@ -34,7 +63,7 @@ const PrivateRoute: React.FC<{ children: React.ReactElement }> = ({ children }) 
     if (!isLogged) {
       const navigateWithDelay = setTimeout(() => {
         navigate('/', { replace: true });
-      }, 1000);
+      }, 1300);
 
       return () => clearTimeout(navigateWithDelay);
     }
@@ -48,7 +77,19 @@ const PrivateRoute: React.FC<{ children: React.ReactElement }> = ({ children }) 
 	return children;
   }
 
-  return <div>You're not logged, return to the login page...</div>;
+  return (
+	<>
+	  <div className="baground">
+	  <div className='containerFullPage'>
+	  	<div style={Container}>
+   		 <div style={Text}>You're not logged, return to the login page...</div>
+		  <img src={LogoutImage} style={Image} alt='logout'/>
+		  </div>
+		  </div>
+		</div>
+	  
+	</>
+  )
 };
 
 export default PrivateRoute;
