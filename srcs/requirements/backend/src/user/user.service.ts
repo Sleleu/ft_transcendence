29c971a2287ca7975e762dce94172fd0e7e6d2b2
sleleu <leleu.sebastien@hotmail.fr>
2023-05-31 01:44:17 +0200

    add new protections pour game login

--- srcs/requirements/backend/src/user/user.service.ts
+++ srcs/requirements/backend/src/user/user.service.ts
@@ -51,24 +51,35 @@ export class UserService {
     }
 
     async updateGameLogin(id: number, gameLogin: string) {
-		console.log("test dans update username", id, gameLogin);
-
-		if (gameLogin.length <= 5) {
-			throw new HttpException('Game login must have more than 5 characters', 400);
-		}
-
-		const existingUser = await this.prismaService.user.findFirst({
-			where: { gameLogin: gameLogin }
-		});
-
-		if(existingUser && existingUser.id !== id){
-			throw new HttpException('Game login is already taken by another user', 400);
-		}
-
+        console.log("test dans update username", id, gameLogin);
+    
+        const existingUser = await this.prismaService.user.findFirst({
+            where: { id: id }
+        });
+        if(!existingUser){
+            throw new HttpException('No user found with given id', 404);
+        }
+        if(existingUser.gameLogin === gameLogin){
+            throw new HttpException('New username is same as the old username', 400);
+        }
+        if (gameLogin.length < 5 || gameLogin.length > 30) {
+            throw new HttpException('Game login must have between 5 and 30 characters', 400);
+        }
+        const validCharacters = /^[a-zA-Z0-9_-]+$/
+        if (!validCharacters.test(gameLogin)) {
+            throw new HttpException('Game login can only contain alphanumeric characters, hyphens and underscores', 400);
+        }
+        const usernameTaken = await this.prismaService.user.findFirst({
+            where: { gameLogin: gameLogin }
+        });
+        if(usernameTaken){
+            throw new HttpException('Game login is already taken by another user', 400);
+        }
         return this.prismaService.user.update({
             where: {id: id},
             data: { gameLogin: gameLogin},
         })
     }
+    
 
 }
